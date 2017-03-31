d7d7ced912fd9f02d55466265796be0b6e8fb125
ユゥ <yuluthi@gmail.com>
2017-03-31 10:42:30 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -24,10 +24,17 @@ print(title)
 print("IPアドレスを取得しています...")
 
 # -----IPアドレスの取得-----
+
+# IP取得用URLの設定
 url = "http://53.u29.xyz/get-ip.php"
-html = urllib.request.urlopen(url)  # URLにアクセスする
-soup = BeautifulSoup(html, 'html.parser')       # htmlをBeautifulSoupで扱う
+# URLにアクセスする
+html = urllib.request.urlopen(url)
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, 'html.parser')
+# 取得したIPアドレスをString型にしておく
 ip_address = str(soup)
+
 # -----IPアドレスの取得終了-----
 
-print("あなたのIPアドレス: " + ip_address)       # htmlのソースを出力
+# htmlのソースを出力
+print("あなたのIPアドレス: " + ip_address)
